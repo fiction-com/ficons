b0a0955be27f6acee0a1040b45c76a0a71d209e1
Andrew Powers <andrew@fiction.com>
2017-11-28 14:03:11 -0700

    improve export

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-module.exports = function(fontName = "ficons") {
-  return {
-    cssFile: `node_modules/ficons/dist/${fontName}/font.min.css`,
-    cssFileRaw: `node_modules/ficons/dist/${fontName}/font.css`,
-    jsonFile: `node_modules/ficons/dist/${fontName}/font.config.json`,
-    json: require(`./dist/${fontName}/font.config.json`)
+module.exports = {
+  config: function(fontName = "ficons") {
+    return {
+      cssFile: `node_modules/ficons/dist/${fontName}/font.min.css`,
+      cssFileRaw: `node_modules/ficons/dist/${fontName}/font.css`,
+      jsonFile: `node_modules/ficons/dist/${fontName}/font.config.json`,
+      json: require(`./dist/${fontName}/font.config.json`)
+    }
   }
 }
